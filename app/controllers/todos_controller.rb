b9ece98d18ace9c2e96357d73b80d98cbd5e4a5c
Leslie Brown <lkb@alum.mit.edu>
2015-07-02 15:26:34 -0400

    Can add todos now

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,14 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def create
+    #Get rid of whitespace
+    params[:todo][:title].strip!   
+    #Only create a new todo if the title isn't empty
+    if params[:todo][:title].length > 0
+      @todo = Todo.create(params.require(:todo).permit(:title))
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  get 'todos/index'
+
+  root 'todos#index'
+
+  resources :todos
+
+#      Prefix Verb   URI Pattern               Controller#Action
+#      root GET    /                         todos#index
+#     todos GET    /todos(.:format)          todos#index
+#           POST   /todos(.:format)          todos#create
+#  new_todo GET    /todos/new(.:format)      todos#new
+# edit_todo GET    /todos/:id/edit(.:format) todos#edit
+#      todo GET    /todos/:id(.:format)      todos#show
+#           PATCH  /todos/:id(.:format)      todos#update
+#           PUT    /todos/:id(.:format)      todos#update
+#           DELETE /todos/:id(.:format)      todos#destroy
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
