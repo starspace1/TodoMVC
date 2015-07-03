b8b4f409a7793296fa0139019bec997b4249fc51
Leslie Brown <lkb@alum.mit.edu>
2015-07-03 15:54:47 -0400

    Created hide_or_show_footer helper method

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,11 @@
 module TodosHelper
+  def hide_or_show_footer
+    # Hide footer if no more todos are left
+    if Todo.count == 0
+      "$('#footer').hide();".html_safe
+    else
+      "$('#footer').show();".html_safe
+    end
+  end
 end
+
