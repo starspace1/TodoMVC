2fda77c4a4eb10e2cf44e21b21a9036bcfd786e5
Leslie Brown <lkb@alum.mit.edu>
2015-07-06 13:57:24 -0400

    All/Active/Completed links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,5 +25,33 @@ $( document ).ready(function() {
     $(this).parent().submit();
   });
 
+  $('#filters').on("click",'a', function(){
+   
+   if ($(this).hasClass('selected')) { return false; }
+
+   var filter = $(this).html();
+
+   console.log(filter)
+   
+   switch (filter)
+   {
+    case 'All':
+      $('#todo-list').find('li').show();
+      break;
+    case 'Active':
+      $('#todo-list').find('li').show();
+      $('#todo-list').find('.completed').hide();
+      break; 
+    case 'Completed':
+      $('#todo-list').find('li').hide();
+      $('#todo-list').find('.completed').show();
+      break;
+    }
+
+    event.preventDefault();
+
+    $('#filters a').removeClass('selected');
+    $(this).addClass('selected');
+  });
 
 });
